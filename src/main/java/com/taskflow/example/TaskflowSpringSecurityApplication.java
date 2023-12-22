70adad1af2078769f10aa5c535ef793c209e93c4
UnesseAh <youness.ahasla@gmail.com>
2023-12-22 15:16:22 +0100

    first commit

---
+++ src/main/java/com/taskflow/example/TaskflowSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.taskflow.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowSpringSecurityApplication.class, args);
+	}
+
+}
