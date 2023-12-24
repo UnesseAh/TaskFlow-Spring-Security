83013116b59149a8d3f73285e76ef85ef4d19988
UnesseAh <youness.ahasla@gmail.com>
2023-12-24 15:41:25 +0100

    define project structure

--- src/main/java/com/taskflow/example/model/User.java
+++ src/main/java/com/taskflow/example/model/User.java
@@ -1,11 +1,12 @@
-package com.taskflow.example.domain;
+package com.taskflow.example.model;
 
 import jakarta.persistence.*;
 
 @Entity
 @Table(name="users")
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstname;
     private String lastname;
