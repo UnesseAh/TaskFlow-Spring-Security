3205bdc212e946479f47f94c5e129d32f6d1c6f3
UnesseAh <youness.ahasla@gmail.com>
2023-12-26 23:51:16 +0100

    create Jwt authentication filter

--- src/main/java/com/taskflow/example/config/JWTAuthenticationFilter.java
+++ src/main/java/com/taskflow/example/config/JWTAuthenticationFilter.java
@@ -1,4 +1,25 @@
 package com.taskflow.example.config;
 
-public class JWTAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+    }
 }
